a80fcca70d924bc2196d705a1870e7bbc1ede9ae
Dan <57277053+d-madden@users.noreply.github.com>
2022-06-11 19:20:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
 
 import './App.css';
@@ -6,12 +7,12 @@ const App = () => {
     return(
         <ChatEngine
             height="100vh"
-            projectID=""
-            userName=""
-            userSecret=""
+            projectID="e711108e-6d03-4362-984a-366119db9fce"
+            userName="danmadden"
+            userSecret="danmadden"
 
         />
-    )
+    );
 }
 
 export default App;
